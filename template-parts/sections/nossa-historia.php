ca603d2792273ed7c247415567aa42385564df0c
Lucas Sampaio <lucas@piucomunica.com.br>
2025-11-22 12:43:23 -0300

    feat: seção nossa história e nossos serviços

--- template-parts/sections/nossa-historia.php
+++ template-parts/sections/nossa-historia.php
@@ -1,3 +1,51 @@
+<?php
+$nossa_historia_tag = get_field('nossa_historia_tag');
+$nossa_historia_titulos = get_field('nossa_historia_titulos');
+$nossa_historia_imagem = get_field('nossa_historia_imagem');
+$nossa_historia_texto = get_field('nossa_historia_texto');
+$nossa_historia_btn_txt = get_field('nossa_historia_btn-txt');
+$nossa_historia_btn_anchor = '#fale-conosco';
+?>
+
 <section class="nossa-historia">
-    
+    <div class="container">
+        <div class="flex gap-10 items-start nossa-historia__header">
+            <div class="tag">
+                <div class="tag-item">
+                    <span><?php echo $nossa_historia_tag; ?></span>
+                </div>
+            </div>
+            <div class="content-text content-text-large">
+                <?php echo $nossa_historia_titulos; ?>
+            </div>
+        </div>
+
+        <div class="grid grid-cols-2 gap-10 nossa-historia__content">
+            <div class="col-span-1 nossa-historia__content-left">
+                <div class="content-image">
+                    <img src="<?php echo esc_url($nossa_historia_imagem['url']); ?>" alt="<?php echo esc_attr($nossa_historia_imagem['alt']); ?>">
+                </div>
+            </div>
+            <div class="col-span-1 nossa-historia__content-right">
+                <div class="content-text content-text-medium">
+                    <?php echo $nossa_historia_texto; ?>
+                </div>
+                <a href="<?php echo $nossa_historia_btn_anchor; ?>" class="btn btn-primary">
+                    <span><?php echo $nossa_historia_btn_txt; ?></span>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="curve-section curve-section--bege-claro">
+        <svg width="40" height="42" viewBox="0 0 40 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 42C0 19.9086 17.9086 2 40 2V0L0 0L0 2L0 42Z" fill="currentColor"/>
+        </svg>
+    </div>
+
+    <div class="curve-section curve-section--azul-petroleo curve-section--br">
+        <svg width="40" height="42" viewBox="0 0 40 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 42C0 19.9086 17.9086 2 40 2V0L0 0L0 2L0 42Z" fill="currentColor"/>
+        </svg>
+    </div>
 </section>
--- template-parts/sections/nossos-servicos.php
+++ template-parts/sections/nossos-servicos.php
@@ -1,3 +1,73 @@
-<section class="nossos-servicos">
-    
+<?php
+$nossos_servicos_header = get_field('nossos_servicos_header');
+$header_tag = $nossos_servicos_header['tag'];
+$header_titulo = $nossos_servicos_header['titulo'];
+$header_subtitulo = $nossos_servicos_header['subtitulo'];
+$nossos_servicos_itens = get_field('nossos_servicos_itens');
+?>
+
+<section class="nossos-servicos section-theme-dark">
+    <div class="nossos-servicos__header">
+        <div class="container">
+            <div class="flex gap-10 items-end justify-between">
+                <div class="w-full max-w-[704px]">
+                    <div class="tag mb-6">
+                        <div class="tag-item">
+                            <span><?php echo $header_tag; ?></span>
+                        </div>
+                    </div>
+                    <div class="content-text">
+                        <h2><?php echo $header_titulo; ?></h2>
+                    </div>
+                </div>
+
+                <div class="w-full max-w-[428px]">
+                    <div class="content-text content-text-large">
+                        <p><?php echo $header_subtitulo; ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="nossos-servicos__itens">
+        <?php foreach ($nossos_servicos_itens as $item) : ?>
+            <div class="nossos-servicos__item">
+                <div class="nossos-servicos__item-content">
+                    <div class="nossos-servicos__item-content-text content-text content-text-medium">
+                        <h3><?php echo $item['servico']; ?></h3>
+                        <p><?php echo $item['descricao']; ?></p>
+                    </div>
+
+                    <div class="nossos-servicos__item-content-destaques">
+                        <?php foreach ($item['destaques'] as $destaque) : ?>
+                            <div class="nossos-servicos__item-content-destaques-item">
+                                <div class="nossos-servicos__item-content-destaques-item-icon">
+                                    <?php echo icon($destaque['icone'], 'thin'); ?>
+                                </div>
+                                <span><?php echo $destaque['texto']; ?></span>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+
+                <div class="nossos-servicos__item-images">
+                    <div class="nossos-servicos__item-images-content">
+                        <?php foreach ($item['imagens'] as $imagem) : ?>
+                            <div class="nossos-servicos__item-images-item">
+                                <img src="<?php echo $imagem['url']; ?>" alt="<?php echo $imagem['alt']; ?>">
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+
+                    <div class="nossos-servicos__item-images-btn">
+                        <a href="#" class="btn btn-secondary">
+                            <span>Ver portfólio</span>
+                            <?php echo icon('images');?>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
 </section>

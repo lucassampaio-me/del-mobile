7f8ce40de7456d4d9d028adca5028184d33fada3
Lucas Sampaio <lucassampaio.me@gmail.com>
2025-12-13 13:38:08 -0300

    commit inicial

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Funções principais do tema DEL Mobile
+ *
+ * Este arquivo carrega todos os componentes necessários para o funcionamento do tema.
+ */
+
+// Configurações básicas do tema
+require_once('inc/core/setup.php');
+
+// Gerenciamento de assets (CSS, JS, bibliotecas)
+require_once('inc/core/assets.php');
+
+// Hooks e filtros personalizados
+require_once('inc/hooks/filters.php');
